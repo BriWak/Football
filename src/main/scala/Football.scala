3bf3e1a6687b0ce26a264c295bd6f59e8319a2e8
Brian Wakenshaw <brian.wakenshaw@digital.hmrc.gov.uk>
2018-03-22 12:36:58 +0000

    remove ArrayBuffers and use Lists

--- src/main/scala/Football.scala
+++ src/main/scala/Football.scala
@@ -1,35 +1,34 @@
-import scala.collection.mutable.ArrayBuffer
 import scala.util.Random
 
 object Football extends App {
-  val teams = Array("Arsenal", "Bournemouth", "Brighton & Hove Albion", "Burnley", "Chelsea",
+  val teams = List("Arsenal", "Bournemouth", "Brighton & Hove Albion", "Burnley", "Chelsea",
     "Crystal Palace", "Everton", "Huddersfield Town", "Leicester City", "Liverpool",
     "Manchester City", "Manchester United", "Newcastle United", "Southampton", "Stoke City",
     "Swansea City")
 
-  val top6 = Array("Arsenal", "Chelsea", "Liverpool", "Manchester City", "Manchester United", "Newcastle United")
+  val top6 = List("Arsenal", "Chelsea", "Liverpool", "Manchester City", "Manchester United", "Newcastle United")
 
-  def round(teams: Array[String]): ArrayBuffer[String] = {
-    val nextRound: ArrayBuffer[String] = new ArrayBuffer
-    val roundNo = teams.length
-    val homeTeams = Random.shuffle(teams.toBuffer).take(teams.length / 2)
-    val awayTeams = teams.filterNot(x => homeTeams.contains(x)).toBuffer
+  def round(teams: List[String]): List[String] = {
     val titleOn = Console.BOLD + Console.RED
-    val homeScores = Seq.fill(teams.length / 2)(Random.nextInt(6))
+    val roundNo = teams.length
 
-    val awayScores = Seq.fill(teams.length / 2)(Random.nextInt(5))
+    val homeTeams : List[String] = Random.shuffle(teams.toBuffer).take(teams.length / 2).toList
+    val awayTeams : List[String] = teams.filterNot(x => homeTeams.contains(x)).toList
+
+    val homeScores : List[Int] = List.fill(teams.length / 2)(Random.nextInt(6))
+    val awayScores : List[Int] = List.fill(teams.length / 2)(Random.nextInt(5))
 
     // zip home scores to the teams and away teams with their scores
     val games = homeTeams.zip(homeScores) zip awayTeams.zip(awayScores)
 
-    val ranked = games.map {
+    val boostTopSix = games.map {
       case ((homeT, hScore), (awayT, aScore)) if top6.contains(homeT) => ((homeT, Random.nextInt(7)), (awayT, aScore))
       case ((homeT, hScore), (awayT, aScore)) if !top6.contains(homeT) => ((homeT, hScore), (awayT, aScore))
       case ((homeT, hScore), (awayT, aScore)) if top6.contains(awayT) => ((homeT, hScore), (awayT, Random.nextInt(7)))
       case ((homeT, hScore), (awayT, aScore)) if !top6.contains(awayT) => ((homeT, hScore), (awayT, aScore))
     }
 
-    val avoidDraws = ranked.map {
+    val avoidDraws = boostTopSix.map {
       case ((homeT, hScore), (awayT, aScore)) if hScore == aScore => ((homeT, hScore+1), (awayT, aScore))
       case ((homeT, hScore), (awayT, aScore)) if hScore != aScore => ((homeT, hScore), (awayT, aScore))
     }
@@ -40,13 +39,13 @@ object Football extends App {
       case ((homeT, hScore), (awayT, aScore)) => println(homeT + " " + hScore + " " + awayT + " " + aScore)
     }
 
-    avoidDraws.foreach {
-      case ((homeT, hScore), (awayT, aScore)) if hScore >= aScore => nextRound.append(homeT)
-      case ((homeT, hScore), (awayT, aScore)) if hScore < aScore => nextRound.append(awayT)
+    val nextRound = avoidDraws.map {
+      case ((homeT, hScore), (awayT, aScore)) if hScore >= aScore => homeT
+      case ((homeT, hScore), (awayT, aScore)) if hScore < aScore => awayT
     }
 
     if (nextRound.length >= 2) {
-      round(nextRound.toArray)
+      round(nextRound)
     }
     else {
       println("\n" + titleOn + "Winner" + Console.RESET + "\n" + nextRound(0))
