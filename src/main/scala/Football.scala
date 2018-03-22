eb694cae01e41f67033505e59bdabc15cfe1b120
Brian Wakenshaw <brian.wakenshaw@digital.hmrc.gov.uk>
2018-03-22 13:02:50 +0000

    minor formatting adjustment

--- src/main/scala/Football.scala
+++ src/main/scala/Football.scala
@@ -13,12 +13,12 @@ object Football extends App {
     val roundNo = teams.length
 
     val homeTeams : List[String] = Random.shuffle(teams.toBuffer).take(teams.length / 2).toList
-    val awayTeams : List[String] = teams.filterNot(x => homeTeams.contains(x)).toList
+    val awayTeams : List[String] = teams.filterNot(x => homeTeams.contains(x))
 
     val homeScores : List[Int] = List.fill(teams.length / 2)(Random.nextInt(6))
     val awayScores : List[Int] = List.fill(teams.length / 2)(Random.nextInt(5))
 
-    // zip home scores to the teams and away teams with their scores
+    // zip home teams to their scores and away teams to their scores
     val games = homeTeams.zip(homeScores) zip awayTeams.zip(awayScores)
 
     val boostTopSix = games.map {
